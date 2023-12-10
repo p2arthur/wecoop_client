3c59753b7dcd50cdf04fcf75c02b75080ab85f1e
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-12-10 19:58:41 -0300

    (feat) start implementing donate button

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useOutletContext, useParams } from 'react-router-dom'
+import Button from '../components/Button'
 import { Feed } from '../services/Feed'
 import { PostProps } from '../services/Post'
 import { User, UserInterface } from '../services/User'
@@ -43,7 +44,7 @@ const ProfilePage = () => {
   return (
     <div className="flex flex-col ">
       <section className="p-4">
-        <div className="w-full h-44 border-2 border-gray-900 p-5">
+        <div className="w-full h-44 border-2 flex flex-col gap-6 border-gray-900 p-5">
           <div className="flex items-center gap-3">
             <div className="border-2 border-gray-900 rounded-full">
               <img className="w-16" src={user?.avatarUri} alt="profile-photo" />
@@ -51,6 +52,10 @@ const ProfilePage = () => {
             <div>
               <h3 className="text-3xl font-bold">{user?.nfd ? user.nfd : ellipseAddress(user?.address)}</h3>
             </div>
+          </div>
+          <div className="flex gap-6 justify-end">
+            <Button buttonText="Donate $COOP" />
+            <Button buttonText="Donate $COOP" />
           </div>
         </div>
       </section>
