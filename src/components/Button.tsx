9f998b08046b63e886cc5130fa274dde4f272c63
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-12-10 20:37:35 -0300

    (styling) improve buttons styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,10 +12,11 @@ const Button = ({ buttonText, buttonFunction, icon, inactive }: ButtonProps) => 
       <button
         className={`${inactive ? 'opacity-30' : 'null'} border-2 border-gray-900
          p-1  bg-white font-bold
-          hover:bg-gray-200 active:bg-gray-300 flex items-center dark:bg-gray-100 dark:text-gray-950 gap-2 border-b-4 active:border-b-transparent active:translate-y-px `}
+          hover:bg-gray-200 active:bg-gray-300 flex items-center dark:border-gray-100 dark:text-gray-100 gap-2 border-b-4 active:border-b-transparent active:translate-y-px dark:border-b-4 dark:hover:bg-gray-800 dark:hover:text-gray-100`}
         onClick={buttonFunction}
         disabled={inactive}
       >
+        {' '}
         {typeof icon === 'string' ? <img className="w-6" src={icon as string} alt="" /> : icon}
         {buttonText}
       </button>
