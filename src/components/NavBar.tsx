b1eb3366e5e596234f790c04fb744c82cfc61816
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-02 04:03:47 -0300

    (fix) fix ui bugs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ interface NavBarProps {
 
 const NavBar = ({ user }: NavBarProps) => {
   return (
-    <div className="px-3 py-0 fixed z-50 bg-gray-100 dark:bg-gray-950 w-screen border-b-4 border-gray-900 flex justify-between items-center h-14">
+    <div className="px-5 py-0 fixed z-50 bg-gray-100 dark:bg-gray-950 w-screen border-b-4 border-gray-900 flex justify-between items-center h-14">
       <a href="/">
         <p className="font-bold text-2xl">WeCOOP</p>
       </a>
