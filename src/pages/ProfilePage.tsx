28ccef6ace1248db3e5414896996168173a1c440
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-02 03:56:47 -0300

    (fix) fix ui bugs

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -77,7 +77,7 @@ const ProfilePage = () => {
   return (
     <div className="flex flex-col ">
       <section className="p-4">
-        <div className="w-full h-44 border-2 flex flex-col gap-6 border-gray-900 dark:border-gray-500 border-b-4 p-5">
+        <div className=" mt-14 w-full h-44 border-2 flex flex-col gap-6 border-gray-900 dark:border-gray-500 border-b-4 p-5">
           <div className="flex gap-3 justify-between items-start">
             <div className="flex flex-col  md:flex-row gap-3">
               <div className="border-2 border-b-4 h-16 w-16 bg-gray-100 border-gray-900 rounded-md overflow-hidden">
