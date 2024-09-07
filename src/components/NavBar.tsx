32bf7563da4aeec70507f17b5dca629da8932913
felipequeiroz <felipe.queiroz@lekto.com.br>
2024-09-06 21:53:16 -0300

    feat: adding about us to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,14 +7,21 @@ interface NavBarProps {
 }
 
 const NavBar = ({ user }: NavBarProps) => {
+  const handleAboutUs = () => {
+    window.location.href = '/about'
+  }
   return (
     <div className="px-5 py-0 fixed z-50 bg-gray-100 dark:bg-gray-950 w-screen border-b-4 border-gray-900 flex justify-between items-center h-14">
       <a className="flex gap-2 items-center" href="/">
         <img className="w-4 md:w-5" src="/images/WeCoop_logo_mascot.svg" alt="wecoop_mascot" />
         <p className="font-bold text-xl md:text-2xl">WeCOOP</p>
       </a>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-5">
+        <a href="/about">
+          <p className="font-bold text-md md:text-md underline">About us</p>
+        </a>
         <ThemeSwitcher />
+
         <ConnectWallet user={user} />
       </div>
     </div>
