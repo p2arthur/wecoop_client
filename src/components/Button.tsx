957ca2a54407f27f7ebe4e62711262619799f7c3
FelipeQueiroz <felipe.queiroz@lekto.com.br>
2024-09-11 18:08:53 -0300

    feat: adding new options to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,31 @@
 interface ButtonProps {
-  buttonText: string
+  buttonText?: string
   icon?: string | React.ReactNode
   inactive?: boolean
   className?: string
   type?: 'button' | 'submit' | 'reset'
   ellipsis?: boolean
+  full?: boolean
+  justify?: 'center' | 'start' | 'end'
 
   buttonFunction?(): void
 }
 
-const Button = ({ buttonText, buttonFunction, icon, inactive, type, ellipsis = false }: ButtonProps) => {
+const Button = ({ buttonText, buttonFunction, icon, inactive, type, justify, full = false, ellipsis = false }: ButtonProps) => {
   return (
     <div>
       <button
         type={type}
         className={`${inactive ? 'opacity-30' : 'null'} border-2 border-gray-900
          p-1  bg-white font-bold
-
+         ${justify && `justify-${justify}`}
+        ${full ? 'w-full ' : 'w-auto'}
           hover:bg-gray-200 active:bg-gray-300 flex items-center dark:border-gray-100 dark:text-gray-100 gap-2 border-b-4 active:border-b-transparent active:translate-y-px dark:border-b-4 dark:hover:bg-gray-800 dark:hover:text-gray-100 text-sm md:text-md`}
         onClick={buttonFunction}
         disabled={inactive}
       >
-        <div> {typeof icon === 'string' ? <img className="w-6" src={icon as string} alt="" /> : icon}</div>
-        <p className={`${ellipsis && 'overflow-hidden truncate w-32'}`}> {buttonText}</p>
+        {icon && <div> {typeof icon === 'string' ? <img className="w-6" src={icon as string} alt="" /> : icon}</div>}
+        {buttonText && <p className={`${ellipsis && 'overflow-hidden truncate w-32'}`}> {buttonText}</p>}
       </button>
     </div>
   )
