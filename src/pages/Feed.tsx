59f7cf3b8a014a118b66b38157bfd0ec64fcd5f6
FelipeQueiroz <felipe.queiroz@lekto.com.br>
2024-09-22 21:21:34 -0300

    fix: fixing post width

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -73,7 +73,7 @@ const Feed = () => {
         />
       </div>
 
-      <div className="p-2 border-2 w-full border-gray-950 dark:border-gray-800 flex flex-col gap-5 h-screen">
+      <div className="p-2 border-2 max-w-[60%] w-full border-gray-950 dark:border-gray-800 flex flex-col gap-5 h-screen">
         <div className=" bg-gray">
           <PostInput />
         </div>
