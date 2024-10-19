30ad01f3751ef05dd600563d93c6cb950b5d45d3
p2arthur <arthurrabelopro@gmail.com>
2024-10-18 17:40:02 -0700

    [upd]

--- src/contracts/app-calls/wecoopDaoMethods.ts
+++ src/contracts/app-calls/wecoopDaoMethods.ts
@@ -136,7 +136,7 @@ export const makeVote = async (
 
     const pollDepositMultiplier = 2
     const pollDepositPrice = Math.floor(baseVotePrice! * pollDepositMultiplier * 10 ** assetDecimals)
-    console.log('poll deposit', pollDepositPrice)
+    console.log('poll deposits', pollDepositPrice)
 
     // Create the asset funding transaction (axfer)
     const axfer = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
