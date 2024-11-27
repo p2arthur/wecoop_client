cce2e71066462e97c3ecc9e91f93848c5bdc767e
p2arthur <arthurrabelopro@gmail.com>
2024-11-26 16:36:26 -0800

    [upd:fix] fix images to algonode instead of pinata

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -118,6 +118,11 @@ const PostCard = ({ post, variant = 'default', handleNewReply, imagesVisible }: 
       const signedTransactions = await signTransactions(encodedGroupedTransactions)
       const waitRoundsToConfirm = 4
 
+      try {
+        const like = await sendTransactions(signedTransactions, waitRoundsToConfirm)
+      } catch (error) {
+        console.error('error', error)
+      }
       const like = await sendTransactions(signedTransactions, waitRoundsToConfirm)
       handleNewLike && handleNewLike({ creator_address: userData?.address || '' }, post.transaction_id as string)
       createLike({
