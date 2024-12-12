32089e79e278e0458ae429683100dff1d23f706e
Yago <yagolobo.dev@gmail.com>
2024-12-12 20:45:42 -0300

    feat: adding pdf  and ppt render components

--- src/components/FileWithLoading/FileWithLoading.tsx
+++ src/components/FileWithLoading/FileWithLoading.tsx
@@ -16,7 +16,7 @@ const FileWithLoading: React.FC<FileWithLoadingProps> = ({ cid, format }) => {
   }
 
   return (
-    <div className="relative w-[400px] h-auto rounded-2xl">
+    <div>
       {isLoading && (
         <div className="inset-0 items-center justify-center flex">
           <img src={!isDarkMode ? '/images/wecoop_loading.gif' : '/images/wecoop_loading_white.gif'} alt={'gif loading'} />
@@ -24,24 +24,46 @@ const FileWithLoading: React.FC<FileWithLoadingProps> = ({ cid, format }) => {
         </div>
       )}
       {format.includes("video") && (
-        <ReactPlayer
-          style={{ opacity: isLoading ? 0 : 1 }}
-          url={`https://ipfs.algonode.xyz/ipfs/${cid}`}
-          controls
-          onReady={handleImageLoad}
-          width={"100%"}
-          height={"auto"}
-        />
+        <div className="relative w-[400px] h-auto rounded-2xl">
+          <ReactPlayer
+            style={{ opacity: isLoading ? 0 : 1 }}
+            url={`https://ipfs.algonode.xyz/ipfs/${cid}`}
+            controls
+            onReady={handleImageLoad}
+            width={"100%"}
+            height={"auto"}
+          />
+        </div>
       )}
       {(format.includes("image") || format === "png") && (
         <img
-          className={`w-full h-auto rounded-2xl ${isLoading ? 'opacity-0' : 'opacity-100'} transition-opacity duration-500`}
+          className={`relative w-[400px] h-auto rounded-2xl ${isLoading ? 'opacity-0' : 'opacity-100'} transition-opacity duration-500 `}
           src={`https://ipfs.algonode.xyz/ipfs/${cid}`}
           onLoad={handleImageLoad}
           alt="IPFS content"
         />
       )
       }
+      {format === "application/pdf" && (
+        <iframe
+          className="relative rounded-2xl"
+          src={`https://ipfs.algonode.xyz/ipfs/${cid}#view=FitH&toolbar=1&navpanes=0`}
+          onLoad={handleImageLoad}
+          width={"100%"}
+          height={"200px"}
+          id="pdf"
+        />
+      )}
+      {format === "application/vnd.ms-powerpoint" && (
+        <iframe
+          src={`https://view.officeapps.live.com/op/embed.aspx?src=https://ipfs.algonode.xyz/ipfs/${cid}`}
+          className="relative w-[400px] rounded-2xl"
+          onLoad={handleImageLoad}
+          width={"100%"}
+          height={"500px"}
+          id="ppt"
+        />
+      )}
     </div>
   )
 }
