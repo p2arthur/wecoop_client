a4590f3a4fbcc87066ac7f183e574caac39a40f6
Arthur Rabelo <arthurrabelopro@gmail.com>
2025-02-11 17:37:11 -0300

    [merge] merge fix profile page using wrong api call

--- src/components/PostInput.tsx
+++ src/components/PostInput.tsx
@@ -303,7 +303,7 @@ const PostInput = ({ postTypeProp = 'post' }: PostInputProps) => {
   const handleCreateFilePost = async () => {
     const country = await getUserCountry()
 
-    console.log('creating file post')
+    console.log('creating file posst')
 
     try {
       setLoadingText('Uploading image to IPFS Network...')
